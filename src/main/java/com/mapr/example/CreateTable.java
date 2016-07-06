3336bc15b943a984cf8328a4a062dc81b12e26c7
Vince Gonzalez <vince@Vinces-MacBook-Pro.local>
2016-07-06 06:30:12 -0700

    initial commit

---
+++ src/main/java/com/mapr/example/CreateTable.java
@@ -0,0 +1,30 @@
+package com.mapr.example;
+
+/**
+ * Created by vince on 7/6/16.
+ */
+
+import java.io.IOException;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.TableName;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class CreateTable {
+
+  public static void main(String[] args) throws IOException {
+    Configuration configuration = HBaseConfiguration.create();
+    HBaseAdmin admin = new HBaseAdmin(configuration);
+    HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf("/tmp/aTable"));
+
+    tableDescriptor.addFamily(new HColumnDescriptor("cf1"));
+    tableDescriptor.addFamily(new HColumnDescriptor("cf2"));
+
+    admin.createTable(tableDescriptor);
+    System.out.println("Table created");
+  }
+}
